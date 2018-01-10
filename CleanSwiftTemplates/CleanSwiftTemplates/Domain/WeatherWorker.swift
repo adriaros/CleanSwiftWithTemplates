cbec4162e4add439d5b2c3dd8d10170b6a4376de
Adria Ros Saez <adria.ros.saez@everis.com>
2018-01-10 16:50:13 +0100

    Added Clean Architecture structure

---
+++ CleanSwiftTemplates/CleanSwiftTemplates/Domain/WeatherWorker.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherWorker.swift
+//  CleanSwiftTemplates
+//
+//  Created by Adria Ros Saez on 1/10/18.
+//  Copyright © 2018 adriaros. All rights reserved.
+//
+
+import Foundation
---
+++ CleanSwiftTemplates/CleanSwiftTemplates/Models/Weather.swift
@@ -0,0 +1,9 @@
+//
+//  Weather.swift
+//  CleanSwiftTemplates
+//
+//  Created by Adria Ros Saez on 1/10/18.
+//  Copyright © 2018 adriaros. All rights reserved.
+//
+
+import Foundation
